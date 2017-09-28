b8d21ed37ebe19d0df7741f0bba6bedfc88725bd
cody <codyjeromegreen@gmail.com>
2017-09-28 14:38:29 -0700

    doctor question

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log('hello there');
 alert('welcome to my guessing game!');
 
 var user = prompt(' what is your name? ');
-alert(' hi ' + user + ' ! I am so glad you want to play my game. ');
+alert(' hi ' + user + ' ! Yay! Ok here we go let\s do this! ');
 
 var answer = prompt(' is Cody\s favorite movie The Princess Bride? Type yes or no. ').toLowerCase();
 console.log(answer);
@@ -19,3 +19,17 @@ if(answer === 'yes' || answer === 'Y' ) {
 }
 
 alert(' you have ' + userPoints + ' points. ');
+console.log(userPoints + ' Points');
+
+var answer2 = prompt(' Is cody\'s favorite show Doctor Who? Type yes or no. ').toLowerCase();
+console.log(answer2);
+
+if(answer2 === 'yes' || answer2 === 'Y' ) {
+  userPoints++;
+  alert( user + ' In 900 years of time and space, I’ve never met anyone who wasn’t important! ' );
+}else{
+  alert(user + ' When you wake up I\'ll be a story in your head. But that\'s okay. We\'re all stories, in the end just make it a good one, eh? Because it was. It was the best.');
+}
+
+alert(' you have ' + userPoints + ' points. ');
+console.log(userPoints + ' Points');
