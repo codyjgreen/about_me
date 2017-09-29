941db47ffc280c5763db239550b2ef260bdc1535
cody <codyjeromegreen@gmail.com>
2017-09-28 18:08:24 -0700

    travel question

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ console.log(answer);
 
 if(answer === 'yes' || answer === 'Y' ) {
   userPoints++;
-  alert( ' inconceivable! Goodnight, ' + user + '. Good work. Sleep well. I\'ll most likely kill you in the morning. ');
+  alert( ' inconceivable! Goodnight, ' + user + '. Good work. Sleep well. I\'ll most likely kill you in the morning. well done! ');
 }else {
-  alert(' My name is Inigo Montoya, you killed my father, prepare to die! ' + user + ' that movie is the best!');
+  alert(' Sorry! My name is Inigo Montoya, you killed my father, prepare to die! ' + user + ' that movie is the best!');
 }
 
 alert(' you have ' + userPoints + ' points. ');
@@ -26,9 +26,25 @@ console.log(answer2);
 
 if(answer2 === 'yes' || answer2 === 'Y' ) {
   userPoints++;
-  alert( user + ' In 900 years of time and space, I’ve never met anyone who wasn’t important! ' );
+  alert( user + ' Yes!  In 900 years of time and space, I’ve never met anyone who wasn’t important! ' );
 }else{
-  alert(user + ' When you wake up I\'ll be a story in your head. But that\'s okay. We\'re all stories, in the end just make it a good one, eh? Because it was. It was the best.');
+  alert(' Sorry this is a great quote though ' + user + ' When you wake up I\'ll be a story in your head. But that\'s okay. We\'re all stories, in the end just make it a good one, eh? Because it was. It was the best.');
+}
+
+alert(' you have ' + userPoints + ' points. ');
+console.log(userPoints + ' Points');
+
+if(userPoints > '2');
+alert('Look at you go!');
+
+var answer3 = prompt(' has cody been out of the country? Type yes or no. ').toLowerCase();
+console.log(answer3);
+
+if(answer3 === 'no' || answer3 === 'N'){
+  userPoints++;
+  alert( ' Sadly ' + user + ' I have not been out of the country but soon! ');
+}else{
+  alert(' sorry but i\'ve never left the country ' + user + ' though im saving up to go to iceland! ');
 }
 
 alert(' you have ' + userPoints + ' points. ');
